97c5b10700bf9353798dfece160b4b07cabc7187
dudu <3375135904@qq.com>
2020-04-17 20:09:54 +0800

    换题功能

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -1,6 +1,6 @@
-import Taro, { Component, Config, request } from '@tarojs/taro'
+import Taro, { Component, Config, cloud } from '@tarojs/taro'
 import { View, Text, } from '@tarojs/components'
-import { AtCheckbox, AtButton  } from 'taro-ui'
+import { AtCheckbox, AtButton, AtModal } from 'taro-ui'
 
 import './index.scss'
 
@@ -24,11 +24,16 @@ export default class Index extends Component {
   constructor () {
     super(...arguments)
     this.state = {
-      answer:1,
-      question:'',
-      checkedList: ['list1'],
-      checkboxOption : [],
-      flag:status.doing
+      questionList:[],
+      currentIndex:0,
+      currentQuestion:{
+        question:'',
+        answer:null,
+        options:[]
+      },
+      checkedList: [],
+      flag:status.doing,
+      isOpened:false
     }
   }
 
@@ -46,16 +51,16 @@ export default class Index extends Component {
   componentWillMount () { }
 
   componentDidMount () { 
-    Taro.cloud
+    const {currentIndex} = this.state;
+    cloud
       .callFunction({
         name: "practices",
       })
       .then(res => {
-        console.log('res: ', res.result.data[0].question)
+        const list = res.result.data;
         this.setState({
-          checkboxOption:res.result.data[0].options,
-          question:res.result.data[0].question,
-          answer:res.result.data[0].answer
+          questionList:list,
+          currentQuestion:list[currentIndex]
         })
       })
   }
@@ -70,25 +75,90 @@ export default class Index extends Component {
     const newValue = value[value.length-1]
     this.setState({
       checkedList: [newValue],
-      flag:this.state.answer === newValue?status.right:status.error
+      flag:this.state.currentQuestion.answer === newValue?status.right:status.error
+    })
+  }
+  handleonpage = () => {
+    const {currentIndex,questionList,checkedList} = this.state; 
+    const index = currentIndex-1;
+    // 存储当前题目选中值
+    questionList[currentIndex].userAnswer = checkedList
+    // 获取上一题的选中值
+    const prevChecked = questionList[index].userAnswer || [];
+    // 获取上一题的选中状态
+    let prevFlag=status.doing;
+    if(questionList[index].userAnswer&&questionList[index].userAnswer[0]) { // 上一题有选中值的情况
+      if(questionList[index].userAnswer[0] === questionList[index].answer) {
+        prevFlag=status.right;
+      } else {
+        prevFlag=status.error;
+      }
+    }
+    this.setState({
+      currentIndex:index,
+      currentQuestion:questionList[index],
+      checkedList:prevChecked,
+      flag:prevFlag
+    })
+  }
+  handlenextpage = () => { 
+    const {currentIndex,questionList,checkedList} = this.state; 
+    const index = currentIndex+1;
+    if(index >= questionList.length){
+      this.setState({
+        isOpened:true
+      })
+      return;
+    }
+    // 存储当前题目选中值
+    questionList[currentIndex].userAnswer = checkedList
+    // 获取下一题的选中值
+    const nextChecked = questionList[index].userAnswer || [];
+    // 获取下一题的选中状态
+    let nextFlag=status.doing;
+    if(questionList[index].userAnswer&&questionList[index].userAnswer[0]) { // 下一题有选中值的情况
+      if(questionList[index].userAnswer[0] === questionList[index].answer) {
+        nextFlag=status.right;
+      } else {
+        nextFlag=status.error;
+      }
+    }
+    this.setState({
+      currentIndex:index,
+      currentQuestion:questionList[index],
+      checkedList: nextChecked,
+      flag:nextFlag,
+    })
+  }
+  handleClose = () => {
+    this.setState({
+      isOpened:false
     })
   }
 
   render () {
-    const {checkboxOption,checkedList,question,flag} = this.state;
+    const {checkedList,flag,currentQuestion,isOpened,currentIndex} = this.state;
     return (
       <View className='index'>
-        <View className='question'>{question}</View>
+        <View className='question'>{currentQuestion.question}</View>
         <AtCheckbox
-        options={checkboxOption}
+        options={currentQuestion.options}
         selectedList={checkedList}
         onChange={this.handleChange.bind(this)}
         />
         <View className='button_box'>
-          <AtButton type='primary' className='button on'>上一题</AtButton>
-          <AtButton type='primary' className='button next'>下一题</AtButton>
+          <AtButton disabled={currentIndex===0} type='primary' className='button on' onClick={this.handleonpage}>上一题</AtButton>
+          <AtButton type='primary' className='button next' onClick={this.handlenextpage}>下一题</AtButton>
         </View>
         <View className='circle' style={{background:flag.bgcolor}}>{flag.text}</View>
+        <AtModal
+          isOpened={isOpened}
+          title='恭喜您'
+          confirmText='朕知道了'
+          onClose={this.handleClose}
+          onConfirm={this.handleClose}
+          content='您已通关啦！！！！'
+        />
       </View>
     )
   }
